3347df6ac4deb4c4cd5699db101864ae65b9bf0b
yeon32 <32jyeon@gamil.com>
2022-10-26 23:43:27 +0900

    Initial Commit

---
+++ SSEUDAMSSEUDAM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSEUDAMSSEUDAM
+//
+//  Created by 김정연 on 2022/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
